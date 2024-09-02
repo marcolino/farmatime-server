c2917e21f57966f657071ab155ee283f07e80bc9
marco <marco@solari@gmail.com>
2024-09-02 09:05:14 +0200

    testing detect-secrets in pre-commit hook

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ const config = require("./src/config");
 const index = "index.html"; // index file name to be injected
 const indexInjected = "index-injected.html"; // injected index file name
 
-const secret = "MY FAKE PASSWORD"; // pragma: allowlist secret
-
 // instantiate express app
 const app = express();
 
