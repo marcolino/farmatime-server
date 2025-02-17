6a072c1bbd70d2e6110d914398409f19707646be
marco <marco@solari@gmail.com>
2025-02-17 11:08:19 +0100

    Debugging github action 'Run Tests' section

--- src/middlewares/passportSetup.js
+++ src/middlewares/passportSetup.js
@@ -11,6 +11,8 @@ const config = require("../config");
 
 console.log("Facebook Client ID:", process.env.FACEBOOK_OAUTH_CLIENT_ID.slice(-10));
 console.log("Facebook Secret Key:", process.env.FACEBOOK_OAUTH_SECRET_KEY.slice(-10));
+console.log("Google Client ID:", process.env.GOOGLE_OAUTH_CLIENT_ID.slice(-10));
+console.log("Google Secret Secret:", process.env.GOOGLE_OAUTH_CLIENT_SECRET.slice(-10));
 
 module.exports = (app) => {
   // Facebook OAuth Strategy
